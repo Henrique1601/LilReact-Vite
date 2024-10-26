234bdf27ed49154dc5bec336dfa4298862bfbbcf
Henrique Bezerra Dos Santos <henriquebs1601@gmail.com>
2024-10-26 18:02:59 -0300

    1.0

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="Main" >
+    <div className="img">
+      <img src="Neon.jpg" alt="" />
+    <div className="container">
+      <div className="title">
+      <h1>Anime: Neon Genesis Evangelion</h1>
+      </div>
+<div className="text">
+    <p>
+    Neon Genesis Evangelion é uma série de anime que se passa em um mundo pós-apocalíptico, onde a humanidade foi dizimada em metade pela catástrofe conhecida como “Segundo Impacto”. A história gira em torno de um grupo de adolescentes que pilotam gigantescas Unidades Evangelion (EVAs) para combater monstros chamados Anjos e evitar o Terceiro Impacto, que destruiria a humanidade.
+    </p>
+</div>
+    <button>Comprar Agora</button>
+    </div>
+    
+    </div>
+    </div>
+  );
+}
+
+export default App;
